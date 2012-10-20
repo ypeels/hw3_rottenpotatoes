18e67b35b4e60c9b7d5ba686114edab84b891afb
ypeels <ypeels@live.com>
2012-10-20 09:02:55 -0700

    took notes on official solution (mainly app/controllers/movie_controller.rb)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,25 +7,27 @@ class MoviesController < ApplicationController
   end
 
   def index
-    sort = params[:sort] || session[:sort]
+    sort = params[:sort] || session[:sort] # a || b == (a != nil ? a : b) - used liberally below to great effect
     case sort
     when 'title'
-      ordering,@title_header = {:order => :title}, 'hilite'
+      ordering,@title_header = {:order => :title}, 'hilite' # double assignment! concise.
     when 'release_date'
       ordering,@date_header = {:order => :release_date}, 'hilite'
     end
     @all_ratings = Movie.all_ratings
-    @selected_ratings = params[:ratings] || session[:ratings] || {}
+    @selected_ratings = params[:ratings] || session[:ratings] || {} # same as my "horrible...hidden state variable"
     
     if @selected_ratings == {}
-      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}] # reproduce params' weird data structure { "G" => "G", etc.}
     end
     
     if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
-      session[:sort] = sort
+      session[:sort] = sort # no unnecessary writes to session (slow?)
       session[:ratings] = @selected_ratings
-      redirect_to :sort => sort, :ratings => @selected_ratings and return
+      redirect_to :sort => sort, :ratings => @selected_ratings and return # doesn't this cause unnecessary redirects?
     end
+    
+    # some nice method-missing action
     @movies = Movie.find_all_by_rating(@selected_ratings.keys, ordering)
   end
 
